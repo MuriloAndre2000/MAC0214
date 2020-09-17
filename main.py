327cc3faa8d4b2b1b153286a55c888c77096b2b5
unknown <muriloandre@bitbucket.org>
2020-09-17 17:49:44 -0300

    Time_Jump

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from ursina import *
+from Game_camera import Time_Travel_Camera
+import time
 
 class Player(Entity):
     def __init__(self, **kwargs):
         super().__init__()
         self.speed = 2.5
         self.jumping = False
-        self.gravity = 5
+        self.gravity = 2
         self.speed_jump = 0.5
         self.time_jumping = 0
 
@@ -16,11 +18,11 @@ class Player(Entity):
             if self.jumping == False:
                 self.jumping = True
         if key == 'q':
-            self.y += 200
-            camera.position.y += 200
+            if self.z<150:
+                self.z += 200
         if key == 'e':
-            self.y -= 200
-            camera.position.y -= 200
+            if self.z> - 150:
+                self.z -= 200
 
     def update(self):
         self.x += held_keys['d'] * time.dt * self.speed
@@ -30,7 +32,9 @@ class Player(Entity):
         if self.jumping:
             #So + Vo*t -g*t^2/2
             self.time_jumping += time.dt
-            new_y = self.speed_jump*self.time_jumping - self.gravity*self.time_jumping*self.time_jumping/2
+            next_time = self.time_jumping + time.dt
+            new_y = self.y+ self.speed_jump*next_time - self.gravity*next_time*next_time/2
+            #print(new_y)
             if new_y > 0:
                 self.y += self.speed_jump*self.time_jumping - self.gravity*self.time_jumping*self.time_jumping/2
             else:
@@ -39,30 +43,26 @@ class Player(Entity):
                 self.time_jumping =0
 
 
-class Cam_Player(Entity):
-    def __init__(self, **kwargs):
-        super().__init__()
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-    def input(self, key):
-        if key == 'q':
-            self.y += 200
-        if key == 'e':
-            self.y -= 200
 
 
 if __name__ == '__main__':
     # window.vsync = False
     app = Ursina()
+    def input(key):
+        if key == 'q':
+            if camera.z<150:
+                camera.z += 200
+        if key == 'e':
+            if camera.z> - 150:
+                camera.z -= 200
+    #app.camera = Time_Travel_Camera()
     Sky(color=color.gray)
-    ground = Entity(model='plane', scale=(5,1,5), color=color.red.tint(-.2), texture='white_cube', texture_scale=(100,100), collider='box')
-    ground = Entity(model='plane', scale=(5,1,5), color=color.blue.tint(-.2), texture='white_cube',position = (0,-200,0), texture_scale=(100,100), collider='box')
-    ground = Entity(model='plane', scale=(5,1,5), color=color.yellow.tint(-.2), texture='white_cube',position = (0,200,0), texture_scale=(100,100), collider='box')
+    ground = Entity(model='plane', scale=(10,1,10), color=color.red.tint(-.2), texture='white_cube', texture_scale=(100,100), collider='box')
+    ground = Entity(model='plane', scale=(10,1,10), color=color.blue.tint(-.2), texture='white_cube',position = (0,0,-200), texture_scale=(100,100), collider='box')
+    ground = Entity(model='plane', scale=(10,1,10), color=color.yellow.tint(-.2), texture='white_cube',position = (0,0,200), texture_scale=(100,100), collider='box')
 
     camera.position = Vec3(-8,6,-8)
     camera.rotation = (30,45,0)
 
     player = Player(model='cube', color=color.dark_gray, scale=(0.25,0.25,0.25), position = (0,0.25,0), collider='box')
-    #print(dir(camera))
-
     app.run()
