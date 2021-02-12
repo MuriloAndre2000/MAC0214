40424860ca0d9e4bbf32df0c95d704b1d0f6c7b5
unknown <muriloandre@bitbucket.org>
2021-02-12 10:56:17 -0300

    now vertical mouse 3 person and textures

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import numpy as np
 import json
 
 time_S_to_wait = 0
-mouse_sensibility = 1
-angle_x = 0
+
 if __name__ == '__main__':
     # window.vsync = False
     angle_x = 0
@@ -27,17 +26,30 @@ if __name__ == '__main__':
     def update():
         #define variables
         player_speed = 2.5
+        radius_horizontal = 8
+        mouse_sensibility = 200
+        print(camera.y)
+        radius_horizontal = math.sqrt(15**2 - (camera.y*camera.y))
         #angle of camera to player
-        sin = (camera.x-player.world_x)/8
-        cos = (camera.z-player.world_z)/8
-        angle_horizontal = math.degrees(math.atan2(sin,cos))
+        sin_horizontal = (camera.x-player.world_x)/radius_horizontal
+        cos_horizontal = (camera.z-player.world_z)/radius_horizontal
+        angle_horizontal = math.degrees(math.atan2(sin_horizontal,cos_horizontal))
+        angle_vertical = math.degrees(math.atan2(camera.y,radius_horizontal))
+
         #player rotate the camera by the player in horizontal way
-        angle_horizontal += held_keys['l']* time.dt *50
-        angle_horizontal -= held_keys['j']* time.dt *50
+        angle_horizontal += mouse.velocity.x * mouse_sensibility
+        angle_vertical += mouse.velocity.y * mouse_sensibility/2
+
+        #angle_vertical_x_y += mouse.velocity.y *200
         angle_horizontal = math.radians(angle_horizontal)
+        angle_vertical = math.radians(angle_vertical)
+
         #define camera position by angle
-        camera.x = player.world_x + 8*math.sin(angle_horizontal)
-        camera.z = player.world_z + 8*math.cos(angle_horizontal)
+        camera.x = player.world_x + radius_horizontal*math.sin(angle_horizontal)
+        camera.z = player.world_z + radius_horizontal*math.cos(angle_horizontal)
+        if radius_horizontal*math.tan(angle_vertical) < 15:
+            if radius_horizontal*math.tan(angle_vertical) > 3:
+                camera.y = radius_horizontal*math.tan(angle_vertical)
         #define vector of player-camera and get x and z
         vector_x = (player.world_position - camera.world_position).x
         vector_z = (player.world_position - camera.world_position).z
@@ -45,32 +57,36 @@ if __name__ == '__main__':
         #will move as 3 person
         if held_keys['w']:
             time_held = held_keys['w'] * time.dt
-            player.world_x += time_held * player_speed * vector_x/8
-            player.world_z += time_held * player_speed * vector_z/8
-            camera.x += time_held * player_speed * vector_x/8
-            camera.z += time_held * player_speed * vector_z/8
+            player.world_x += time_held * player_speed * vector_x/radius_horizontal
+            player.world_z += time_held * player_speed * vector_z/radius_horizontal
+            camera.x += time_held * player_speed * vector_x/radius_horizontal
+            camera.z += time_held * player_speed * vector_z/radius_horizontal
         if held_keys['s']:
             time_held = held_keys['s'] * time.dt
-            player.world_x -= time_held * player_speed * vector_x/8
-            player.world_z -= time_held * player_speed * vector_z/8
-            camera.x -= time_held * player_speed * vector_x/8
-            camera.z -= time_held * player_speed * vector_z/8
+            player.world_x -= time_held * player_speed * vector_x/radius_horizontal
+            player.world_z -= time_held * player_speed * vector_z/radius_horizontal
+            camera.x -= time_held * player_speed * vector_x/radius_horizontal
+            camera.z -= time_held * player_speed * vector_z/radius_horizontal
         if held_keys['d']:
             time_held = held_keys['d'] * time.dt
-            player.world_x += time_held * player_speed * vector_z/8
-            player.world_z -= time_held * player_speed * vector_x/8
-            camera.x += time_held * player_speed * vector_z/8
-            camera.z -= time_held * player_speed * vector_x/8
+            player.world_x += time_held * player_speed * vector_z/radius_horizontal
+            player.world_z -= time_held * player_speed * vector_x/radius_horizontal
+            camera.x += time_held * player_speed * vector_z/radius_horizontal
+            camera.z -= time_held * player_speed * vector_x/radius_horizontal
         if held_keys['a']:
             time_held = held_keys['a'] * time.dt
-            player.world_x -= time_held * player_speed * vector_z/8
-            player.world_z += time_held * player_speed * vector_x/8
-            camera.x -= time_held * player_speed * vector_z/8
-            camera.z += time_held * player_speed * vector_x/8
+            player.world_x -= time_held * player_speed * vector_z/radius_horizontal
+            player.world_z += time_held * player_speed * vector_x/radius_horizontal
+            camera.x -= time_held * player_speed * vector_z/radius_horizontal
+            camera.z += time_held * player_speed * vector_x/radius_horizontal
         camera.look_at(player, axis='forward')
     #app.camera = Time_Travel_Camera()
     Sky(color=color.white)
-    ground = Entity(model='terrain', scale=(0.1,.1,.1), texture_scale=(100,100), collider='box')
+    mouse.locked = True
+    print(dir(mouse))
+    window.fullscreen = True 
+    ground = Entity(model='models/terrain_simple', scale=(1,1,1), texture = 'models/texture/dirt')
+    stone = Entity(model='models/stone1', scale=(.05,.05,.05), position =(3.5,1,3), texture = 'models/texture/stone')
     ground = Entity(model='plane', scale=(10,1,10), color=color.blue.tint(-.2), texture='white_cube',position = (0,0,-200), texture_scale=(100,100), collider='box')
     ground = Entity(model='plane', scale=(10,1,10), color=color.yellow.tint(-.2), texture='white_cube',position = (0,0,200), texture_scale=(100,100), collider='box')
     #print(dir(ground))
